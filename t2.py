fcbdd053d4efdd0068b90d04caf7a2291f72e130
Amadeus <OlyaSlobodyanuk@yandex.ru>
2016-07-05 19:36:32 +0300

    Второе задание выполнено

---
+++ t2.py
@@ -0,0 +1,10 @@
+import random
+import sys
+n = int(sys.argv[1])
+sum = 0
+for i in range(0, n):
+	r = random.uniform(-1, 1)
+	print("%.4f" % r)
+	sum += r
+	
+print("������� �������� = %.4f" % (sum / n))
